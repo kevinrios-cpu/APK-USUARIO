14cfc3d22fd0ac8ecbc08894e75de6abe137d6c5
kevinrios-cpu <80782564+kevinrios-cpu@users.noreply.github.com>
2022-07-02 19:37:55 -0400

    toda la info conectada a la fake-api-js

--- Detalle-de-usuario/src/app/data/services/api/user.service.ts
+++ Detalle-de-usuario/src/app/data/services/api/user.service.ts
@@ -24,4 +24,22 @@ export class UserService extends ApiClass{
       catchError(this.error)
     );
   }
+
+  /** get one user by id*/
+  getUserById(id: number ): Observable<{
+    error: boolean,
+    msg: string,
+    data: ICardUser
+  }> {
+    const response = { error: false, msg: '', data: null};
+    return this.http.get<ICardUser>(this.url + 'users/' + id)
+    .pipe(
+      map( r => {
+        response.data = r;
+        return response;
+      }
+    ),
+    catchError(this.error)
+    )
+  }
 }
